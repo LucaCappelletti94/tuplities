550225d1c20293672e77ceb0d4762f46f32652a6
Luca <cappelletti.luca94@gmail.com>
2025-12-10 08:04:36 +0100

    Added missing impl for unit tuple

--- tuplities-flatten-nest/src/nested_try_from.rs
+++ tuplities-flatten-nest/src/nested_try_from.rs
@@ -42,6 +42,13 @@ where
     }
 }
 
+impl<E> NestedTupleTryFrom<(), E> for () {
+    #[inline]
+    fn nested_tuple_try_from(_other: ()) -> Result<Self, E> {
+        Ok(())
+    }
+}
+
 impl<Head, OtherHead, E> NestedTupleTryFrom<(OtherHead,), E> for (Head,)
 where
     Head: TryFrom<OtherHead>,
@@ -107,5 +114,47 @@ mod tests {
         let source = (1u8, (2u8,));
         let target: Result<(u16, (u16,)), MyError> = source.nested_tuple_try_into();
         assert_eq!(target, Ok((1u16, (2u16,))));
+    }
+
+    // Note: We avoid using `TryFromIntError` in these tests to improve portability
+    // and to focus on custom error types that do not implement `From<Infallible>`.
+
+    #[derive(Debug, PartialEq)]
+    struct WrapErr;
+
+    struct WrapI16(i16);
+
+    impl TryFrom<WrapI16> for u8 {
+        type Error = WrapErr;
+
+        fn try_from(val: WrapI16) -> Result<u8, WrapErr> {
+            if let Ok(val) = u8::try_from(val.0) {
+                Ok(val)
+            } else {
+                Err(WrapErr)
+            }
+        }
+    }
+
+    #[test]
+    fn test_unit_tuple_try_from() {
+        let source = ();
+        let target: Result<(), WrapErr> = NestedTupleTryFrom::nested_tuple_try_from(source);
+        assert_eq!(target, Ok(()));
+    }
+
+    #[test]
+    fn test_nested_tuple_try_from_custom_wrapper_error_single() {
+        let source = (WrapI16(-1),);
+        let target: Result<(u8,), WrapErr> = NestedTupleTryFrom::nested_tuple_try_from(source);
+        assert!(target.is_err());
+    }
+
+    #[test]
+    fn test_nested_tuple_try_from_custom_wrapper_error_nested() {
+        let source = (WrapI16(256), (WrapI16(1), (WrapI16(2),)));
+        let target: Result<(u8, (u8, (u8,))), WrapErr> =
+            NestedTupleTryFrom::nested_tuple_try_from(source);
+        assert!(target.is_err());
     }
 }
